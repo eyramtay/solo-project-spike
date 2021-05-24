b75a2d23f92e46d5ae0db879971a06fa608db56c
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-23 23:24:45 -0500

    added nav bar buttons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,19 @@ function App() {
   return (
     <div>
       <h1>Solo-Project-Spike!</h1>
+      <div className="navBar">
+        <ul>
+          <li><button>Sign Out</button></li>
+          <li><button>Events</button></li>
+          <li><button>Add Restaurant</button></li>
+          <li><button>Home</button></li>
+        </ul>
+      </div>
+
+    <div>
+      
+    </div>  
+
     </div>
   );
 }
